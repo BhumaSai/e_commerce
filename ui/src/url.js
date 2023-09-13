303e946cc5cf1a32cbe277f26880ca428065e43f
Bhuma_Sai <bhumasairam123@gmail.com>
2023-09-13 14:27:09 +0530

    url changed

--- ui/src/url.js
+++ ui/src/url.js
@@ -1,10 +1,8 @@
 import axios from 'axios'
 
 export const URL = axios.create({
-    baseURL: 'http://localhost:4000/',
+    baseURL: 'https://ecommercebe.onrender.com/',
     withCredentials: true
-    // baseURL: 'https://ecommercebe.onrender.com/',
-
 })
 
-export const IMGURL = 'http://localhost:4000/'
+export const IMGURL = 'https://ecommercebe.onrender.com/'
