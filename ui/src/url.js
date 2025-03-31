a021d3a0e552eab34c1181a28a13fc592bb9ee10
BhumaSai <bhumasairam123@gmail.com>
2025-03-31 22:30:28 +0530

    fixed cookie issue

--- ui/src/url.js
+++ ui/src/url.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const URL = axios.create({
-  // baseURL: "https://ecommercebe.onrender.com",
-  baseURL: "http://localhost:4000/",
+  baseURL: "https://ecommercebe.onrender.com",
+  // baseURL: "http://localhost:4000/",
   withCredentials: true,
 });
 
